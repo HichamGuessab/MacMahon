e0276596044202173adba93ef2bf93e327310a4a
Hicham Guessab <hichamgsb.pro@gmail.com>
2023-10-29 20:02:03 +0100

    Affichage d'un plateau (avec lecture d'un fichier)

--- plateau.cpp
+++ plateau.cpp
@@ -7,13 +7,17 @@ Plateau::Plateau(vector<string> fichier) {
     this->fichier_ = fichier;
     this->rows_ = fichier[0][0] - '0';
     this->columns_ = fichier[0][2] - '0';
-    this->plateau_ = vector<vector<Tuile>>(this->rows_, vector<Tuile>());
+    this->plateau_ = vector<vector<Tuile>>();
 }
 
 void Plateau::remplir() {
-    for (int i = 1; i < this->rows_ + 1; i++) {
-        for (int j = 0; j < this->columns_; j++) {
-            this->plateau_[i - 1].emplace_back('G', 'R', 'B', 'R');
+    vector<Tuile> lignesTuile = vector<Tuile>();
+    for (int i = 1; i < (this->rows_ * this->columns_) + 1; i++) {
+        Tuile tuile = Tuile(this->fichier_[i][0], this->fichier_[i][2], this->fichier_[i][4], this->fichier_[i][6]);
+        lignesTuile.push_back(tuile);
+        if (i % this->columns_ == 0) {
+            this->plateau_.push_back(lignesTuile);
+            lignesTuile.clear();
         }
     }
 }
