4d03ec88f179ff73e26e75a0ad6f5fdfc96d0f5a
Hicham Guessab <hichamgsb.pro@gmail.com>
2023-11-10 13:28:54 +0100

    Ajustements

--- file/file.h
+++ file/file.h
@@ -11,7 +11,7 @@ public:
     explicit File(string path);
     vector<string>readFile();
 private:
-    string path = "/Users/hicham/Library/Mobile Documents/com~apple~CloudDocs/Documents/University/M1_ILSEN/UE_Conception_Logicielle/Programmation_Parrallele/MacMahon/boardsTxt/";
+    string path = "../boardsTxt/";
 };
 
 #endif // MACMAHON_FILE_H
